4510b30aa3fbb34d8978b04c97f536b1e1b30aaf
NguyenHuuLap <85687467+NguyenHuuLap@users.noreply.github.com>
2024-01-04 23:11:48 +0700

    Delete sellshop_be directory

--- sellshop_be/src/config/assets/image_path.js
+++
@@ -1,9 +0,0 @@
-const WebAssets = () => {
-  const imgLogin = "./assets/images/Shipper_CPS3.png";
-  const imgLogo = "";
-  return {
-    imgLogin,
-    imgLogo,
-  };
-};
-export default WebAssets;
--- sellshop_be/src/utils/NumberFormat.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-export default function NumberFormat({ number }) {
-  const options = { maximumFractionDigits: 2 };
-  const formattedNumber = Intl.NumberFormat("en-US", options).format(number);
-
-  return formattedNumber;
-}
